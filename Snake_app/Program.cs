4ec5fa729852a39557ea7d284bf7423a6038c0f9
marina.oleinik <marina.oleinik@LENOVO-PF14MM6L>
2020-03-24 10:23:06 +0200

    2 käsud oli lisatud

--- Snake_app/Program.cs
+++ Snake_app/Program.cs
@@ -7,6 +7,8 @@ namespace Snake_app
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
